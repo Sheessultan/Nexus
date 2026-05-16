5d23a8b376c884a653edfec8612869dc9c35bb96
ᴘʀιɴcᴇ ԍᴀɴԍwᴀʀ <112890795+prince-567@users.noreply.github.com>
2026-05-16 22:58:52 +0530

    old remove

--- console/apps/api/src/console/console.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConsoleGateway } from './console.gateway';
-import { PowershellParserService } from './powershell-parser.service';
-
-@Module({
-  providers: [ConsoleGateway, PowershellParserService],
-})
-export class ConsoleModule {}
